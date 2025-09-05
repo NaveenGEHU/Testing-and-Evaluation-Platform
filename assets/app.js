7a25dff7f6dabe346a9c19fcd11e08ee721f53ce
Naveen <naveensinghnegiwork@gmail.com>
2025-09-05 11:25:43 +0530

    I have defined the module for application

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,35 @@
-function change(){
-            document.getElementById("btn").innerHTML="clicked";
-        }
+class Student {
+    public:
+    constructor(name, rollNo, section,studentid,password,email) {}
+}
+
+class accesor{
+    constructor(name,email,password,){
+
+    }
+}
+
+class question{
+    constructor(statement,option1,option2, option3 ,option4,answer) {}
+}
+
+class questionPaper{
+    constructor(question [ ] , answerkey[])
+}
+
+
+function register()
+function login()
+function logout()
+function addQuestion()
+function showeQuestion()
+function addanswer()
+function sortbymarks()
+function sortbyname()
+function sortbysection()
+function sortbyrollno()
+function searchbymarks()
+function searchbyname()
+function searchbysection()
+function searchbyrollno()
+
